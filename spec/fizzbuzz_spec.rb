ae9766b32ebe0a6cc71b89b49af29a1071b5cd49
Marek <MarekSchmidt@outlook.com>
2018-07-09 01:37:26 +0100

    Third test passing

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,7 +7,13 @@ describe 'fizzbuzz(3)' do
 end
 
 describe 'fizzbuzz(1)' do
-  it 'returns "1" when passed 1' do
+  it 'returns 1 when passed 1' do
     expect(fizzbuzz(1)).to eq 1
+  end
+end
+
+describe 'fizzbuzz(5)' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
 end
